da16ace6486cc2f72e3a36118d50dada24036bfb
Adrian <hlmkadri@gmail.com>
2018-09-19 08:34:51 +0200

    mi primer commit

---
+++ app-old.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+http.createServer((req, res) => {
+
+        res.writeHead(200, { "content-type": "application/json" });
+
+        let salida = {
+            nombre: "adrian",
+            edad: 29,
+            url: req.url
+        }
+
+        res.write(JSON.stringify(salida));
+        // res.write("Hola mundo");
+        res.end();
+    })
+    .listen(8080);
+
+console.log("Escuchando el puerto 8080");
